f445e4e8848cf2f0caf10e9388b39f1850500c31
TalFo <talfolkman11@gmail.com>
2014-12-19 17:18:43 +0200

    try555

--- script.py
+++ script.py
@@ -9,7 +9,6 @@ start_date = datetime.date(2010, 1, 1)
 delta = datetime.timedelta(days=random.randint(1,5))
 
 while start_date <= end_date:
-    # print(start_date)
     date = f"{start_date} 11:18:43 +0200"
     print(date)
     os.environ['GIT_AUTHOR_DATE'] = date
